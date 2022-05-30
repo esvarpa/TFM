43b19315ae1511be8cb81f585c2bfd7693dd9f9e
esvarpa <esvarpa@uoc.edu>
2022-05-29 22:07:13 -0500

    cambios

--- ui.R
+++ ui.R
@@ -85,25 +85,4 @@ ui <- tagList(
     )
   )
 
-options(shiny.maxRequestSize = 60*1024^2)
 
-# Define server 
-server <- function(input, output, session) {
-  
-  source("server-inputData.R",local = TRUE)
-  # 
-  source("server-enrichGo.R",local = TRUE)
-  # 
-  source("server-plots.R",local = TRUE)
-  
-  
-  GotoTab <- function(name){
-    #updateTabItems(session, "tabs", name)
-    
-    shinyjs::show(selector = paste0("a[data-value=\"",name,"\"]"))
-    
-    shinyjs::runjs("window.scrollTo(0, 0)")
-  }
-}
-
-shinyApp(ui, server)
